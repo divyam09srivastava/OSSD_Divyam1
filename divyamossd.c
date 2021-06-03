d1f9f5440169791b3de2db8ce24de79d23815553
Divyam Shrivastava <59364676+divyam09srivastava@users.noreply.github.com>
2021-06-03 20:34:34 +0530

    divyamossd.c added

---
+++ divyamossd.c
@@ -0,0 +1,32 @@
+# include <stdio.h>
+# include <string.h>
+   
+int main( )
+{
+    FILE *filePointer ;    
+    char dataToBeWritten[50] 
+        = "Divyam SHRIVASTAVA";
+  
+    filePointer = fopen("GfgTest.c", "w") ;
+    
+    if ( filePointer == NULL )
+    {
+        printf( "GfgTest.c file failed to open." ) ;
+    }
+    else
+    {
+          
+        printf("The file is now opened.\n") ;
+        if ( strlen (  dataToBeWritten  ) > 0 )
+        {
+            fputs(dataToBeWritten, filePointer) ;   
+            fputs("\n", filePointer) ;
+        }
+
+        fclose(filePointer) ;
+          
+        printf("Data successfully written in file DivyamTest.c\n");
+        printf("The file is now closed.") ;
+    }
+    return 0;        
+}
